f153eab1b1d1c9fdda283b28b36903e965ab03bd
Masato Ikami <User@User-no-MacBook-Air.local>
2014-11-04 15:27:33 +0800

    Initial Commit

---
+++ SamplePList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SamplePList
+//
+//  Created by Masato Ikami on 2014/11/04.
+//  Copyright (c) 2014年 Masato Ikami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SamplePList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SamplePList
+//
+//  Created by Masato Ikami on 2014/11/04.
+//  Copyright (c) 2014年 Masato Ikami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
